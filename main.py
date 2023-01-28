51e96425ea29d3c92e4663c1108097f7eefe76a4
HighDimension Cost <46214170+cv-numpy@users.noreply.github.com>
2023-01-28 13:15:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Feature obj
-
 import numpy as np
 from cv2 import VideoCapture
 from cv2 import bitwise_or
@@ -8,10 +6,11 @@ from cv2 import bitwise_or
 import detect
 import fn2
 import palm
-import finger
+import finger_layer_iter
 import draw_index
 
-class Feature:
+
+class PointsIndex:
     def __init__(self, camera_index, framework_name = 'mediapipe'):
         self.camX = camera_index
         camera = VideoCapture(self.camX)
@@ -34,8 +33,11 @@ class Feature:
 
         if framework_name == 'mediapipe':
             self.using_framework = framework_name
-            from map1 import marksX
-            self.marksX = marksX
+            # from map1 import marksX
+            # self.marksX = marksX
+
+            self.finger = dict()
+            self.finger_layer = dict()
 
     # get 2d infomation
     def scalars(self):
@@ -55,17 +57,32 @@ class Feature:
                 numpy_xy = np.array(self.origin_xy)
                 self.pixel_xy = numpy_xy * self.square_size
 
-                # get "distance" between camera and "Human Hand"
-                self.distance = palm.points_distance(self.pixel_xy)
-
+                # Palm
+                self.clock_wise, self.deepth_distance, self.horizontals_lengths, self.virtical_length, self.middle_point, self.vector, self.palm_trangle_area = palm.update_palm(self.pixel_xy)
+                self.t_ratio = self.virtical_length / self.horizontals_lengths
                 # Hand ROI
-                self.frame_crop = fn2.crop(frame, self.pixel_xy, self.distance)
+                self.frame_crop = fn2.crop(frame, self.pixel_xy, self.deepth_distance)
 
-                self.raio = palm.t_icon(self.pixel_xy)
-                self.clock_wise = palm.clock_wise_check(self.pixel_xy)
-                self.palm_middle, self.head_vector = palm.pointing(self.pixel_xy)
-                self.diffs_2d, self.fingers_middles = finger.finger_layers(self.pixel_xy)
-                self.ratios = finger.diffs_ratio_13_23(self.diffs_2d)
+
+                # Finger
+                thumb, index_finger, middle_finger, ring_finger, little_finger = finger_layer_iter.fingers_a(self.pixel_xy)
+
+                self.finger['thumb'] = thumb
+                self.finger['index'] = index_finger
+                self.finger['middle'] = middle_finger
+                self.finger['ring'] = ring_finger
+                self.finger['litte'] = little_finger
+
+                # Finger Layers
+                layer1, layer2, layer3, layer4 = finger_layer_iter.layers(self.pixel_xy)
+
+                self.finger_layer['head'] = layer1
+                self.finger_layer['head_middle'] = layer2
+                self.finger_layer['end_middle'] = layer3
+                self.finger_layer['end_middle'] = layer4
+
+                self.diffs_2d, self.fingers_middles = finger_layer_iter.finger_layers(self.pixel_xy)
+                self.ratios = finger_layer_iter.diffs_ratio_13_23(self.diffs_2d)
             else:
                 pass
 
@@ -74,14 +91,22 @@ class Feature:
 
     def get_mask(self):
         if self.number == 1:
-            mask_stick = draw_index.stick_mask(self.pixel_xy, self.distance)
-            mask_palm = draw_index.palm_mask(self.pixel_xy, self.distance)
+            mask_stick = draw_index.stick_mask(self.pixel_xy, self.deepth_distance)
+            mask_palm = draw_index.palm_mask(self.pixel_xy, self.deepth_distance)
             self.mask = bitwise_or(mask_stick, mask_palm)
+
+    def neighbor_line(self):
+        self.two_points = dict()
+        self.two_points['thumb'] = zip( self.finger['thumb'][:-1], self.finger['thumb'][1:] )
+        self.two_points['index'] = zip( self.finger['index'][:-1], self.finger['index'][1:] )
+        self.two_points['middle'] = zip( self.finger['middle'][:-1], self.finger['middle'][1:] )
+        self.two_points['ring'] = zip( self.finger['ring'][:-1], self.finger['ring'][1:] )
+        self.two_points['little'] = zip( self.finger['little'][:-1], self.finger['little'][1:] )
 
 
 
 def test():
-    real1 = Feature(0)
+    real1 = PointsIndex(0)
     import cv2 as cv
     cv.imshow('Demo', real1.demo_hand)
     cv.waitKey(1500)
@@ -89,14 +114,17 @@ def test():
     if real1.camera_ready:
         import time
         time1 = time.time(); time2 = time1
+        t_ratios = []
         while time2 - time1 < 30:
             real1.scalars()
             real1.get_mask()
             if real1.number == 1:
+                t_ratios.append(real1.t_ratio)
                 cv.imshow('Demo', real1.mask)
 
             if cv.waitKey(1) == ord('q'):
                 break
+            time2 = time.time()
 
     real1.camera.release()
 
