f2afa00a79cc6c82f66d62b2ed30c97da2a7382a
HighDimension Cost <46214170+cv-numpy@users.noreply.github.com>
2023-01-28 13:29:09 +0800

    Update what.py

--- what.py
+++ what.py
@@ -1,8 +1,3 @@
-# connect_frames.py
-'''
-connect_frames.py
-'''
-
 # dashboard.py
 '''
 Build a 2D dashboard of hand for animation.
@@ -16,11 +11,6 @@ Using the model of Google MediaPipe hand detection
 # draw_index.py
 '''
 draw_function
-'''
-
-# edge.py
-'''
-edge.py
 '''
 
 # finger.py
@@ -40,124 +30,14 @@ then do the math calculation about the points.
 Functions used to ajust image and window size to let them fit.
 '''
 
-# fn3.py
-'''
-fn3.py
-'''
-
-# fn4.py
-'''
-fn4.py
-'''
-
-# foo.py
-'''
-foo.py
-'''
-
-# hand_streamlit.py
-'''
-hand_streamlit.py
-'''
-
-# hand.py
-'''
-hand.py
-'''
-
-# index_time.py
-'''
-index_time.py
-'''
-
-# kernel.py
-'''
-kernel.py
-'''
-
-# kmean_cnt.py
-'''
-kmean_cnt.py
-'''
-
-# layer_iter_test.py
-'''
-layer_iter_test.py
-'''
-
-# layer_iter.py
-'''
-layer_iter.py
-'''
-
 # map1.py
 '''
 Build a HashMap dictionary to name the finger joints' index for the map of landmarks predicted by Convolutional Neural Network.
 '''
 
-# map2.py
-'''
-map2.py
-'''
-
-# matrix_rain_opencv.py
-'''
-matrix_rain_opencv.py
-'''
-
-# matrix_rain_with_mask.py
-'''
-matrix_rain_with_mask.py
-'''
-
-# matrix_rain.py
-'''
-matrix_rain.py
-'''
-
-# measure.py
-'''
-measure.py
-'''
-
-# Morphological_simpling.py
-'''
-Morphological_simpling.py
-'''
-
 # palm.py
 '''
 Try to draw a "mask" of palm for opencv "bitwise_and" operation.
-'''
-
-# roi.py
-'''
-roi.py
-'''
-
-# skin.py
-'''
-skin.py
-'''
-
-# steps.py
-'''
-steps.py
-'''
-
-# streamlit_demo.py
-'''
-streamlit_demo.py
-'''
-
-# threshold_different_size.py
-'''
-threshold_different_size.py
-'''
-
-# threshold.py
-'''
-threshold.py
 '''
 
 # u8.py
